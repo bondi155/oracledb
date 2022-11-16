51f956d398a29c2ae4b39c89e9a29f9b02db648b
bondi155 <78815412+bondi155@users.noreply.github.com>
2022-11-15 19:26:29 -0600

    xml file falta loop commit

--- controllers/dbquerys.js
+++ controllers/dbquerys.js
@@ -1,12 +1,12 @@
-const dbConfig = require ('../config/dbconfig');
+const dbConfig = require('../config/dbconfig');
 const oracledb = require('oracledb');
-var o2x = require('object-to-xml');
-
+const { create } = require('xmlbuilder2');
+const fs = require('fs');
 
 //conexion a instant client       -----------0------------------
 if (process.platform === 'win32') {
   try {
-    oracledb.initOracleClient({libDir: 'C:\\oracle\\instantclient_12_2'});   // doble comilla
+    oracledb.initOracleClient({ libDir: 'C:\\oracle\\instantclient_12_2' }); // doble comilla
   } catch (err) {
     console.error('error de conexion con instant client..');
     console.error(err);
@@ -17,57 +17,70 @@ if (process.platform === 'win32') {
 oracledb.autoCommit = true;
 process.env.ORA_SDTZ = 'UTC';
 
-
-async function getData (request, response) {
-  
+async function getData(request, response) {
   //conexion a base de datos
   oracledb.getConnection(dbConfig, function (err, connection) {
     if (err) {
       console.error(err.message);
-      response.status(500).send("Error connecting to DB");
+      response.status(500).send('Error connecting to DB');
       return;
     }
 
-    console.log("conexion establecida ejecutando query...")
-    
+    console.log('conexion establecida ejecutando query...');
+
     const sql = `SELECT * FROM personas`;
-    
-    connection.execute( sql ,{},
+
+    connection.execute(
+      sql,
+      {},
       { outFormat: oracledb.OBJECT },
       (err, result) => {
         if (err) {
           console.error(err.message);
-          response.status(500).send("Error getting data from DB");
+          response.status(500).send('Error getting data from DB');
           return;
         }
 
-        console.log("RESULTSET:" + JSON.stringify(result));
-        var EDI_DC40 = [];
-        result.rows.forEach( (element) => {
-          EDI_DC40.push({ id:element.ID, nombre:element.NOMBRE, 
-                           pais: element.PAIS});
+        console.log('RESULTSET:' + JSON.stringify(result));
+
+        let EDI_DC40 = [];
+
+        result.rows.forEach((element) => {
+          EDI_DC40.push({
+            id: element.ID,
+            nombre: element.NOMBRE,
+            pais: element.PAIS,
+          });
         }, this);
-        response.set('Content-Type', 'text/xml');
-        response.send (o2x({
-          '?xml version="1.0" encoding="utf-8"?' :null,
-          MBGMCR03 :{
-            IDOC :{ 
-              EDI_DC40
-            }
-          }
-       
+        console.log(EDI_DC40);
 
+        const root = create({ version: '1.0', encoding: 'UTF-8' })
+          .ele('MBGMCR03')
+          .ele('IDOC')
+          .ele('EDI_DC40')
+          .ele('id')
+          .txt(`${EDI_DC40[0].id}`)
+          .up()
+          .ele('nombre')
+          .txt(`${EDI_DC40[0].nombre}`)
+          .up()
+          .ele('pais')
+          .txt(`${EDI_DC40[0].pais}`)
+          .up()
+          .up();
 
-        }))
+        const xml = root.end({ prettyPrint: true });
+        console.log(xml);
 
-    
-      });
+        fs.writeFile('prueba.xml', xml, (err) => {
+          if (err) throw err;
+          console.log('archivo XML creado');
+        });
+      }
+    );
   });
-  
- 
 }
 
 module.exports = {
-
-   getData:getData
-}
+  getData: getData,
+};
--- server.js
+++ server.js
@@ -4,31 +4,10 @@ var bodyParser = require('body-parser');
 const dbquery = require ('./controllers/dbquerys');
 const PORT = process.env.PORT || 8089;
 
-
 // configure app to use bodyParser()
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ type: '*/*' }));
 
 
-var router = express.Router();
-
-router.use(function (request, response, next) {
-  console.log("REQUEST:" + request.method + "   " + request.url);
-  console.log("BODY:" + JSON.stringify(request.body));
-  response.setHeader('Access-Control-Allow-Origin', '*');
-  response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  response.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
-
- app.post('/personas/', dbquery.getData);
-
-
-app.use(express.static('static'));
-app.use('/', router);
-
-app.listen( PORT, () =>{
-  console.log("Servidor corriendo en puerto " + PORT)
-} )  
+dbquery.getData();
 
