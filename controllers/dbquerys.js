db96aeec9725b80f478cb2850ce9d6ff70f67ab8
bondi155 <78815412+bondi155@users.noreply.github.com>
2022-11-18 10:09:28 -0600

    commit antes reinicio

--- controllers/dbquerys.js
+++ controllers/dbquerys.js
@@ -1,93 +1,110 @@
-const dbConfig = require('../config/dbconfig');
-const oracledb = require('oracledb');
-var xml2js = require('xml2js');
-const fs = require('fs');
+process.env.ORA_SDTZ = 'UTC';
 
-//conexion a instant client       -----------0------------------
-if (process.platform === 'win32') {
-  try {
-    oracledb.initOracleClient({ libDir: 'C:\\oracle\\instantclient_12_2' }); // doble comilla
-  } catch (err) {
-    console.error('error de conexion con instant client..');
-    console.error(err);
-    process.exit(1);
-  }
-}
-//                   ----------------o----------------
+const fs = require('fs');
+const oracledb = require('oracledb');
+const dbConfig = require('../config/dbconfig');
+const xml2js = require('xml2js');
+
 oracledb.autoCommit = true;
 process.env.ORA_SDTZ = 'UTC';
 
-async function getData (request, response) {
-  //conexion a base de datos
-  oracledb.getConnection(dbConfig, function (err, connection) {
-    if (err) {
-      console.error(err.message);
-      response.status(500).send('Error connecting to DB');
-      return;
-    }
-      
-    console.log('conexion establecida ejecutando query...');
+async function getData() {
+  let connection;
 
-    const sqlSelect = `SELECT * FROM personas where estado IS NULL`;
+  try {
+    let sql, binds, options, result;
 
-  
-    connection.execute(
-      sqlSelect,
-      {},
-      { outFormat: oracledb.OBJECT },
-      (err, result) => {
-        if (err) {
-          console.error(err.message);
-          response.status(500).send('Error getting data from DB');
-          return;
-        }
+    //conexion a db com dbconfig
 
-        console.log('RESULTSET:' + JSON.stringify(result));
+    connection = await oracledb.getConnection(dbConfig);
 
-        let querySelect = [];
+    // Select
+    sql = ` SELECT * FROM personas where estado = 'pendiente'`;
+
+    result = await connection.execute(sql, {}, { outFormat: oracledb.OBJECT });
+    console.log('RESULTSET:' + JSON.stringify(result));
+
+    let querySelect = [];
+
+    result.rows.forEach((element) => {
+      querySelect.push({
+        IDOC: {
+          EDI_DC40: {
+            id: element.ID,
+            nombre: element.NOMBRE,
+            pais: element.PAIS,
+           status: element.ESTADO,
+          },
+        },
+        E1MBGMCR: {},
+      });
+    }, this);
+
+    var builder = new xml2js.Builder({
+      explicitRoot: false,
+      rootName: 'MBGMCR03',
+    });
+
+    var xml = builder.buildObject(querySelect);
+    console.log(xml);
+
+    let status = querySelect.map((status) => status.IDOC.EDI_DC40.status);
+    console.log(status);
+
+    if (typeof querySelect != "undefined" && querySelect != null && querySelect.length != null && querySelect.length > 0) { 
+
+    fs.writeFile('prueba.xml', xml, (err) => {
+      if (err) throw err;
+      console.log('archivo XML creado');
+    });
+  }
+
+    let ids = querySelect.map((id) => id.IDOC.EDI_DC40.id);
+   
 
 
-        result.rows.forEach((element) => {
-          querySelect.push({
-            IDOC: {
-              EDI_DC40: {
-                id: element.ID,
-                nombre: element.NOMBRE,
-                pais: element.PAIS,
-                estado: element.ESTADO,
-              },
-            },
-            E1MBGMCR: {},
-          });
-        }, this);
+    console.log(ids);
 
-        let ids = querySelect.map (id => id.IDOC.EDI_DC40.id);
+    
+    // UPDATE cargado a los id que se hicieron select 
+    
 
-      
+  //  sql = `UPDATE personas SET estado = 'cargado' WHERE id = 11133`;
 
+    binds = {}
 
+    // For a complete list of options see the documentation.
+    options = {
+      // extendedMetaData: true,               // get extra metadata
+      // prefetchRows:     100,                // internal buffer allocation size for tuning
+      // fetchArraySize:   100                 // internal buffer allocation size for tuning
+    };
 
-        let sqlUpdate = `UPDATE personas SET estado = 'cargado' WHERE id = :id`;
-        const binds = {id: ids };
+    result = await connection.execute(sql, binds, options);
 
-       
-        var builder = new xml2js.Builder({
-          explicitRoot: false,
-          rootName: 'MBGMCR03',
-        });
-        var xml = builder.buildObject(querySelect);
-        console.log(xml);
+   
+    //
+    // Show the date.  The value of ORA_SDTZ affects the output
+    //
 
-        fs.writeFile('prueba.xml', xml, (err) => {
-          if (err) throw err;
-          console.log('archivo XML creado');
-        });
+    sql = `SELECT TO_CHAR(CURRENT_DATE, 'DD-Mon-YYYY HH24:MI') AS CD FROM DUAL`;
+    result = await connection.execute(sql);
+    console.log('Current date query results: ');
+    console.log(result.rows[0]);
+
+  } catch (err) {
+    console.error(err);
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();
+      } catch (err) {
+        console.error(err);
       }
-    );
-  });
+    }
+  }
 }
 
 module.exports = {
   getData: getData,
 };
-
